b399bc7f01fa1550da7c9ecaf8682a9fcfc879ad
Michael O. <mcvavy@gmail.com>
2025-07-14 18:26:16 +0100

    native only issue

---
+++ app/summarycopy.tsx
@@ -0,0 +1,18 @@
+// import { StripeProvider } from '@stripe/stripe-react-native';
+
+// import Payment  from "./components/Payment";
+
+// export default function SummaryScreen() {
+
+//   const publishableKey = process.env.EXPO_PUBLIC_STRIPE_PUBLISHABLE_KEY;
+
+//   return (
+//     <StripeProvider
+//       publishableKey={publishableKey!}
+//       merchantIdentifier="merchant.com.vavy.tranzrmoves" // required for Apple Pay
+//       urlScheme="tranzr-moves" // required for 3D Secure and bank redirects
+//     >
+//       <Payment />
+//     </StripeProvider>
+//   );
+// }
